6949e89efbd3aee9c47ec90f3f73d23681a40314
Max Mikhanosha <max@openchat.com>
2012-03-02 19:38:07 -0500

    Fix several things:
    1) When SLIME sends an interrupt while executing stuff in the GUI
       thread, interrupt the right thread
    
    2) Wrap evaluation in (with-connection)
    
    3) Bind *package* to whatever it was is the original repl thread, rather then
       using global value, before passing it back
    
    4) Remove REPL eval hook, if GUI thread dies, so that REPL does not become
       completely non-functional

--- repl-integration.lisp
+++ repl-integration.lisp
@@ -11,10 +11,13 @@
 (defvar *notifier*)
 (defvar *gui-thread*)
 (defvar *executer*)
+(defvar *in-gui-thread* nil)
 
 (defclass repl-notifier ()
   ((pending-form :accessor pending-form)
+   (emacs-connection :accessor emacs-connection)
    (form-result :accessor form-result)
+   (old-package :accessor old-package)
    (new-package :accessor new-package))
   (:metaclass qt-class)
   (:qt-superclass "QObject")
@@ -24,12 +27,32 @@
                                        &key &allow-other-keys)
   (new repl-notifier))
 
+#+swank
+(defvar *old-interrupt-worker-thread*
+  (fdefinition 'swank::interrupt-worker-thread))
+
+#+swank
+(defun swank::interrupt-worker-thread (id)
+  "When Swank tries to interrupt repl-thread, and repl-thread
+is using Qt signal (emit ...) and is currently waiting for
+gui thread to finish, interrupt the gui thread instead where
+the actual form is being evaluated"
+  (if (and (eq id :repl-thread)
+           (boundp '*in-gui-thread*)
+           *in-gui-thread*)
+      (bt:interrupt-thread *gui-thread*
+                           (lambda () (error "Interrupt from Emacs")))
+      (funcall *old-interrupt-worker-thread* id)))
+
 (defun notifier-do-eval (notifier)
   (flet ((doit ()
-           (setf (form-result notifier)
-                 (multiple-value-list
-                     (eval (pending-form notifier)))
-                 (new-package notifier) *package*)))
+           (let ((*package* (old-package notifier)))
+             (setf (form-result notifier)
+                   (multiple-value-list
+                    #+swank (swank::with-connection ((emacs-connection notifier)) 
+                              (eval (pending-form notifier)))
+                    #-swank (eval (pending-form notifier)))
+                   (new-package notifier) *package*))))
     #-swank
     (doit)
     #+swank
@@ -55,8 +78,16 @@
 (defun %eval-in-gui-thread (notifier form)
   (unwind-protect
        (progn
-         (setf (pending-form notifier) form)
-         (emit-signal notifier "formReady()")
+         (setf (pending-form notifier) form
+               (old-package notifier) *package*)
+         #+swank (setf (emacs-connection notifier) swank::*emacs-connection*)
+         ;; We use global rather then per-thread binding, because
+         ;; slime interrupt is not called from REPL thread but rather
+         ;; from control thread
+         (unwind-protect
+             (progn (setf *in-gui-thread* t)
+                    (emit-signal notifier "formReady()"))
+           (setf *in-gui-thread* nil))
          (cond ((slot-boundp notifier 'form-result)
                 (setf *package* (new-package notifier))
                 (apply #'values (form-result notifier)))
@@ -85,41 +116,52 @@
     *print-readably* *print-right-margin*
     *package*))
 
-(defun start-gui-thread (&optional (install-repl-hook t))
+(defun start-gui-thread (&optional (install-repl-hook t) (expander #'identity))
   (unless (boundp '*gui-thread*)
     (ensure-smoke :qtcore)
     (ensure-smoke :qtgui)
-    (setf *notifier* (make-instance 'repl-notifier)
-          *gui-thread*
-          (let ((global-values (mapcar #'symbol-value *globals*)))
-            (flet ((setup-and-exec ()
+    (setf *notifier* (make-instance 'repl-notifier))
+    (setf 
+     *gui-thread*
+     (let ((global-values (mapcar #'symbol-value *globals*))
+           (hooks (find-symbol "*SLIME-REPL-EVAL-HOOKS*" "SWANK")))
+       (labels
+           ((hook (form)
+              (%eval-in-gui-thread *notifier* (funcall expander form)))
+            (setup-and-exec ()
+              (unwind-protect
+                   (progn
                      (loop for var in *globals*
                            for value in global-values
                            do (setf (symbol-value var) value))
+                     (when (and install-repl-hook (find-package "SWANK"))
+                       (if hooks
+                           (push #'hook (symbol-value hooks))
+                           (warn "Cannot initialize *SLIME-REPL-EVAL-HOOKS*, use (eval-in-gui-thread ...) form.")))
                      (setf *qapp* (make-qapplication)
                            *executer* (make-instance 'repl-executer
-                                                     :notifier *notifier*))
+                                       :notifier *notifier*))
                      (#_setQuitOnLastWindowClosed *qapp* nil)
                      (loop
-                       (#_exec *qapp*))))
-              #+(and darwin sbcl)
-              (let ((initial-thread (find "initial thread"
-                                          (bt:all-threads)
-                                          :test #'string-equal
-                                          :key #'bt:thread-name)))
-                (if (eq initial-thread (bt:current-thread))
-                    (setup-and-exec)
-                    (bt:interrupt-thread initial-thread #'setup-and-exec))
-                initial-thread)
-              #+(and darwin (not sbcl))
-              (error "sorry, don't know how to find the initial thread. FIXME.")
-              #-darwin
-              (bt:make-thread #'setup-and-exec))))
-    (when (and install-repl-hook (find-package "SWANK"))
-      (let ((hooks (find-symbol "*SLIME-REPL-EVAL-HOOKS*" "SWANK")))
-        (if hooks
-            (push #'(lambda (form)
-                      (%eval-in-gui-thread *notifier* form))
-                  (symbol-value (find-symbol "*SLIME-REPL-EVAL-HOOKS*" "SWANK")))
-            (warn "Cannot initialize *SLIME-REPL-EVAL-HOOKS*, use (eval-in-gui-thread ...) form."))
-        (values)))))
+                       (#_exec *qapp*)))
+                (warn "QT Gui thread exited")
+                (if hooks
+                    (set hooks (delete #'hook (symbol-value hooks))))
+                (makunbound '*gui-thread*)
+                (#_delete *executer*)
+                (#_delete *notifier*)
+                (makunbound '*executer*)
+                (makunbound '*notifier*))))
+         #+(and darwin sbcl)
+         (let ((initial-thread (find "initial thread"
+                                     (bt:all-threads)
+                                     :test #'string-equal
+                                     :key #'bt:thread-name)))
+           (if (eq initial-thread (bt:current-thread))
+               (setup-and-exec)
+               (bt:interrupt-thread initial-thread #'setup-and-exec))
+           initial-thread)
+         #+(and darwin (not sbcl))
+         (error "sorry, don't know how to find the initial thread. FIXME.")
+         #-darwin
+         (bt:make-thread #'setup-and-exec :name "qt-repl-thread"))))))
